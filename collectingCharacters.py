a3631864792dff5278e91c5cb50a6203162430b3
kukretipranshu <kukretis26@gmail.com>
2023-02-02 21:43:39 +0530

    importing libraries and defining functions

--- collectingCharacters.py
+++ collectingCharacters.py
@@ -3,6 +3,45 @@ import string
 import numpy as np
 import os
 import cv2
+import imutils
+import mediapipe as mp
+import uuid
+
+# Setting up hand points variable and landmark drawing variable
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+# Processing the input image
+def process_image(img):
+
+    # Converting the input to grayscale
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(gray_image)
+
+    # Returning the detected hands to call function
+    return results
+
+# Drawing landmark connections
+def draw_hand_connections(img, results):
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+
+                # Finding the coordinates of each landmark
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                # Printing each landmark ID and coordinates
+                # on the terminal
+                print(id, cx, cy)
+
+                # Drawing the landmark connections
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+        return img
+
 
 # Creating Necessary Directory Structure for the Image Dataset
 
